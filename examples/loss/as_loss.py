76fd7a622ccf4e5c9f1c5fee8f07255ec2628aca
jm12138 <2286040843@qq.com>
2022-02-22 02:17:06 +0800

    update loss example

--- examples/loss/as_loss.py
+++ examples/loss/as_loss.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 import sys, os
 import paddle.nn.functional as F
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '..'))
 from paddle_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 npImg1 = np.array(Image.open("einstein.png"))
 
@@ -21,14 +21,16 @@ ssim_loss = SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, ch
 
 optimizer = Adam(parameters=[img2], learning_rate=0.01)
 
+step = 0
 while ssim_value < 0.9999:
+    step += 1
     optimizer.clear_grad()
     _ssim_loss = 1-ssim_loss(img1, img2)
     _ssim_loss.backward()
     optimizer.step()
 
     ssim_value = ssim(img1, img2).item()
-    print(ssim_value)
+    print('step: %d ssim_loss: %f' % (step, ssim_value))
 
 img2_ = (img2 * 255.0).squeeze()
 np_img2 = img2_.detach().numpy().astype(np.uint8)
