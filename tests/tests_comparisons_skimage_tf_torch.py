5f328ec0479fffefaf3aca666847cb2a021b3b78
jm12138 <2286040843@qq.com>
2022-02-22 01:52:39 +0800

    add torch test

--- tests/tests_comparisons_skimage_tf_torch.py
+++ tests/tests_comparisons_skimage_tf_torch.py
@@ -11,6 +11,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from paddle_msssim import ssim, ms_ssim
 import paddle
 import tensorflow as tf
+import torch
+import pytorch_msssim
+
 
 if __name__ == '__main__':
     img = Image.open('kodim10.png')
@@ -20,8 +23,10 @@ if __name__ == '__main__':
     ##########
     #   SSIM
     ##########
-    img_batch = []
-    img_noise_batch = []
+    img_batch_paddle = []
+    img_noise_batch_paddle = []
+    img_batch_torch = []
+    img_noise_batch_torch = []
     single_image_ssim = []
     print('===================================')
     print("             Test SSIM")
@@ -50,30 +55,47 @@ if __name__ == '__main__':
         img_paddle = paddle.to_tensor(img).unsqueeze(0).transpose((0, 3, 1, 2))  # 1, C, H, W
         img_noise_paddle = paddle.to_tensor(img_noise).unsqueeze(0).transpose((0, 3, 1, 2))
 
-        img_batch.append(img_paddle)
-        img_noise_batch.append(img_noise_paddle)
+        img_batch_paddle.append(img_paddle)
+        img_noise_batch_paddle.append(img_noise_paddle)
 
         begin = time.time()
         for _ in range(N_repeat):
             ssim_paddle = ssim(img_noise_paddle, img_paddle, win_size=11, data_range=255)
         time_paddle = (time.time()-begin) / N_repeat
 
+        img_torch = torch.from_numpy(img).unsqueeze(0).permute(0, 3, 1, 2)  # 1, C, H, W
+        img_noise_torch = torch.from_numpy(img_noise).unsqueeze(0).permute(0, 3, 1, 2)
+
+        img_batch_torch.append(img_torch)
+        img_noise_batch_torch.append(img_noise_torch)
+
+        begin = time.time()
+        for _ in range(N_repeat):
+            ssim_torch = pytorch_msssim.ssim(img_noise_torch, img_torch, win_size=11, data_range=255)
+        time_torch = (time.time()-begin) / N_repeat
+
+        ssim_torch = ssim_torch.numpy()
         ssim_paddle = ssim_paddle.numpy()
         single_image_ssim.append(ssim_paddle)
         ssim_tf = ssim_tf.numpy()
 
-        print("sigma=%.1f ssim_skimage=%.6f (%.4f ms), ssim_tf=%.6f (%.4f ms), ssim_paddle=%.6f (%.4f ms)" % (
-            sigma, ssim_skimage, time_skimage*1000, ssim_tf, time_tf*1000, ssim_paddle, time_paddle*1000))
+        print("sigma=%.1f ssim_skimage=%.6f (%.4f ms), ssim_tf=%.6f (%.4f ms), ssim_paddle=%.6f (%.4f ms), ssim_torch=%.6f (%.4f ms)" % (
+            sigma, ssim_skimage, time_skimage*1000, ssim_tf, time_tf*1000, ssim_paddle, time_paddle*1000, ssim_torch, time_torch*1000))
         assert (np.allclose(ssim_paddle, ssim_skimage, atol=5e-4))
         assert (np.allclose(ssim_paddle, ssim_tf, atol=5e-4))
+        assert (np.allclose(ssim_paddle, ssim_torch, atol=5e-4))
 
     print("Pass!")
 
     print("====> Batch")
-    img_batch = paddle.concat(img_batch, axis=0)
-    img_noise_batch = paddle.concat(img_noise_batch, axis=0)
-    ssim_batch = ssim(img_noise_batch, img_batch, win_size=11,size_average=False, data_range=255)
-    assert np.allclose(ssim_batch.numpy(), np.concatenate(single_image_ssim, 0), atol=5e-4)
+    img_batch_paddle = paddle.concat(img_batch_paddle, axis=0)
+    img_noise_batch_paddle = paddle.concat(img_noise_batch_paddle, axis=0)
+    ssim_batch_paddle = ssim(img_noise_batch_paddle, img_batch_paddle, win_size=11,size_average=False, data_range=255)
+    img_batch_torch = torch.cat(img_batch_torch, dim=0)
+    img_noise_batch_torch = torch.cat(img_noise_batch_torch, dim=0)
+    ssim_batch_torch = pytorch_msssim.ssim(img_noise_batch_torch, img_batch_torch, win_size=11,size_average=False, data_range=255)
+    assert np.allclose(ssim_batch_paddle.numpy(), np.concatenate(single_image_ssim, 0), atol=5e-4)
+    assert np.allclose(ssim_batch_paddle.numpy(), ssim_batch_torch.numpy(), atol=5e-4)
     print("Pass!")
 
     ##########
@@ -113,26 +135,43 @@ if __name__ == '__main__':
             msssim_paddle = ms_ssim(img_noise_paddle, img_paddle, win_size=11, data_range=255)
         time_paddle = (time.time()-begin) / N_repeat
 
+        img_torch = torch.from_numpy(img).unsqueeze(0).permute(0, 3, 1, 2)  # 1, C, H, W
+        img_noise_torch = torch.from_numpy(img_noise).unsqueeze(0).permute(0, 3, 1, 2)
+
+        begin = time.time()
+        for _ in range(N_repeat):
+            msssim_torch = pytorch_msssim.ms_ssim(img_noise_torch, img_torch, win_size=11, data_range=255)
+        time_torch = (time.time()-begin) / N_repeat
+
+        msssim_torch = msssim_torch.numpy()
         msssim_paddle = msssim_paddle.numpy()
         msssim_tf = msssim_tf.numpy()
 
-        print("sigma=%.1f msssim_tf=%.6f (%.4f ms), msssim_paddle=%.6f (%.4f ms)" % (
-            sigma, msssim_tf, time_tf*1000, msssim_paddle, time_paddle*1000))
+        print("sigma=%.1f msssim_tf=%.6f (%.4f ms), msssim_paddle=%.6f (%.4f ms), msssim_torch=%.6f (%.4f ms)" % (
+            sigma, msssim_tf, time_tf*1000, msssim_paddle, time_paddle*1000, msssim_torch, time_torch*1000))
         assert (np.allclose(msssim_tf, msssim_paddle, atol=5e-4))
+        assert (np.allclose(msssim_torch, msssim_paddle, atol=5e-4))
 
     print("Pass")
     print("====> Batch")
-    _img_batch= np.stack( img_batch, axis=0 )
-    _img_noise_batch = np.stack( img_noise_batch, axis=0 )
+    img_batch= np.stack(img_batch, axis=0)
+    img_noise_batch = np.stack(img_noise_batch, axis=0)
 
-    img_batch = paddle.to_tensor(_img_batch).transpose((0, 3, 1, 2))
-    img_noise_batch = paddle.to_tensor(_img_noise_batch).transpose((0, 3, 1, 2))
-    msssim_paddle_batch = ms_ssim(img_noise_batch, img_batch, win_size=11, size_average=False, data_range=255)
+    img_batch_paddle = paddle.to_tensor(img_batch).transpose((0, 3, 1, 2))
+    img_noise_batch_paddle = paddle.to_tensor(img_noise_batch).transpose((0, 3, 1, 2))
+    msssim_batch_paddle = ms_ssim(img_noise_batch_paddle, img_batch_paddle, win_size=11, size_average=False, data_range=255)
 
-    img_batch = tf.convert_to_tensor( _img_batch)
-    img_noise_batch = tf.convert_to_tensor(_img_noise_batch)
-    msssim_tf_batch = tf.image.ssim_multiscale( img_noise_batch, img_batch, 255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03  )
-    assert np.allclose(msssim_paddle_batch.numpy().reshape(-1), msssim_tf_batch.numpy().reshape(-1), atol=5e-4)
+    img_batch_torch = torch.from_numpy(img_batch).permute(0, 3, 1, 2)
+    img_noise_batch_torch = torch.from_numpy(img_noise_batch).permute(0, 3, 1, 2)
+    msssim_batch_torch = pytorch_msssim.ms_ssim(img_noise_batch_torch, img_batch_torch, win_size=11, size_average=False, data_range=255)
+
+    img_batch_tf = tf.convert_to_tensor(img_batch)
+    img_noise_batch_tf = tf.convert_to_tensor(img_noise_batch)
+    msssim_batch_tf = tf.image.ssim_multiscale( img_noise_batch_tf, img_batch_tf, 255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
+
+    assert np.allclose(msssim_batch_paddle.numpy().reshape(-1), msssim_batch_tf.numpy().reshape(-1), atol=5e-4)
+    assert np.allclose(msssim_batch_paddle.numpy().reshape(-1), msssim_batch_torch.numpy().reshape(-1), atol=5e-4)
+
     print("Pass")
 
 
