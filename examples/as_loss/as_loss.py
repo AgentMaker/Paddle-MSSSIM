3b5cbd746f25320ab90a50886d53bc7818713215
jm12138 <2286040843@qq.com>
2022-02-22 02:28:20 +0800

    add example loss switch

--- examples/as_loss/as_loss.py
+++ examples/as_loss/as_loss.py
@@ -6,6 +6,18 @@ import sys, os
 import paddle.nn.functional as F
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '..'))
 from paddle_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+
+loss_type = 'msssim'
+assert loss_type in ['ssim', 'msssim']
+
+if loss_type == 'ssim':
+    loss = ssim
+    loss_obj = SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=1)
+else:
+    loss = ms_ssim
+    loss_obj = MS_SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=1)
+
+
 npImg1 = np.array(Image.open("einstein.png"))
 
 img1 = paddle.to_tensor(npImg1).unsqueeze(0).unsqueeze(0) / 255.0
@@ -14,10 +26,8 @@ img2 = paddle.rand(img1.shape)
 img1 = paddle.to_tensor(img1, stop_gradient=True)
 img2 = paddle.to_tensor(img2, stop_gradient=False)
 
-ssim_value = ssim(img1, img2).item()
+ssim_value = ms_ssim(img1, img2).item()
 print("Initial ssim:", ssim_value)
-
-ssim_loss = SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=1)
 
 optimizer = Adam(parameters=[img2], learning_rate=0.01)
 
@@ -25,12 +35,12 @@ step = 0
 while ssim_value < 0.9999:
     step += 1
     optimizer.clear_grad()
-    _ssim_loss = 1-ssim_loss(img1, img2)
+    _ssim_loss = 1-loss_obj(img1, img2)
     _ssim_loss.backward()
     optimizer.step()
 
-    ssim_value = ssim(img1, img2).item()
-    print('step: %d ssim_loss: %f' % (step, ssim_value))
+    ssim_value = loss(img1, img2).item()
+    print('step: %d %s_loss: %f' % (step, loss_type, ssim_value))
 
 img2_ = (img2 * 255.0).squeeze()
 np_img2 = img2_.detach().numpy().astype(np.uint8)
