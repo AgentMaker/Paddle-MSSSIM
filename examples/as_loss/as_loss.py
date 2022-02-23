9dd28f0a1bfd3f82e5779de33227af8c80a93aee
jm12138 <2286040843@qq.com>
2022-02-23 17:02:25 +0800

    update as loss example

--- examples/as_loss/as_loss.py
+++ examples/as_loss/as_loss.py
@@ -6,30 +6,29 @@ import numpy as np
 from PIL import Image
 from paddle.optimizer import Adam
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '..'))
-from paddle_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+from paddle_msssim import SSIM, MS_SSIM
 
 
-loss_type = 'msssim'
+loss_type = 'ssim'
 assert loss_type in ['ssim', 'msssim']
 
 if loss_type == 'ssim':
-    loss = ssim
-    loss_obj = SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=1)
+    loss_func = SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=1)
 else:
-    loss = ms_ssim
-    loss_obj = MS_SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=1)
+    loss_func = MS_SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=1)
 
 
-npImg1 = np.array(Image.open("einstein.png"))
+np_img1 = np.array(Image.open("einstein.png"))
 
-img1 = paddle.to_tensor(npImg1).unsqueeze(0).unsqueeze(0) / 255.0
+img1 = paddle.to_tensor(np_img1).unsqueeze(0).unsqueeze(0) / 255.0
 img2 = paddle.rand(img1.shape)
 
 img1 = paddle.to_tensor(img1, stop_gradient=True)
 img2 = paddle.to_tensor(img2, stop_gradient=False)
 
-ssim_value = ms_ssim(img1, img2).item()
-print("Initial ssim:", ssim_value)
+with paddle.no_grad():
+    ssim_value = loss_func(img1, img2).item()
+    print("Initial %s: %f:" % (loss_type, ssim_value))
 
 optimizer = Adam(parameters=[img2], learning_rate=0.01)
 
@@ -37,13 +36,15 @@ step = 0
 while ssim_value < 0.9999:
     step += 1
     optimizer.clear_grad()
-    _ssim_loss = 1-loss_obj(img1, img2)
-    _ssim_loss.backward()
+    loss = loss_func(img1, img2)
+    (1-loss).backward()
     optimizer.step()
 
-    ssim_value = loss(img1, img2).item()
-    print('step: %d %s_loss: %f' % (step, loss_type, ssim_value))
+    ssim_value = loss.item()
+    if step % 10 == 0:
+        print('Step: %d %s_loss: %f' % (step, loss_type, ssim_value))
 
-img2_ = (img2 * 255.0).squeeze()
-np_img2 = img2_.detach().numpy().astype(np.uint8)
-Image.fromarray(np_img2).save('results.png')
+img2 = (img2 * 255.0).squeeze()
+np_img2 = img2.detach().numpy().astype(np.uint8)
+print('Save the result image to result.png')
+Image.fromarray(np_img2).save('result.png')
